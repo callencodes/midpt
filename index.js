12117241142ae6904449c6162984fdf18ff20919
callencodes <callencoding@gmail.com>
2022-04-15 15:36:09 -0400

    specify port

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.get('/', (req,res) => {
   res.send('Midpt Homepage')
 })
 
 
-app.listen(3000, () => (
-  console.log(`Listening on 3000`)
+app.listen(port, () => (
+  console.log(`Listening on ${port}`)
 ))
